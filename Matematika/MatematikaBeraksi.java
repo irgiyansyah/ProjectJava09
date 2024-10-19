32957c0e12391422d024615512796d32f4950153
Irgi-Dev <147305943+irgiyansyah@users.noreply.github.com>
2024-10-19 11:01:35 +0800

    Update MatematikaBeraksi.java

--- Matematika/MatematikaBeraksi.java
+++ Matematika/MatematikaBeraksi.java
@@ -1,5 +1,5 @@
 package matematika;
-
+// Menggunakan Class MatematikaBeraksi sebagai Main fungsi
 public class MatematikaBeraksi {
     public static void main(String[] args) {
         // Membuat objek Matematika
@@ -8,14 +8,14 @@ public class MatematikaBeraksi {
         // Mengeksekusi method dan menampilkan hasilnya
         int hasilPertambahan = mtk.pertambahan(20, 20);
         System.out.println("Pertambahan: 20 + 20 = " + hasilPertambahan);
-
+        //  Mengesekusi Method dan menampilkan hasil pengurangan dari algoritma
         int hasilPengurangan = mtk.pengurangan(10, 5);
         System.out.println("Pengurangan: 10 - 5 = " + hasilPengurangan);
-
+        // Mengesekusi Method dan menampilkan hasil product dari algoritma java
         int hasilPerkalian = mtk.perkalian(10, 20);
         System.out.println("Perkalian: 10 * 20 = " + hasilPerkalian);
-
+        // Mengekekusi Method dan menampilkan hasil product dari algoritma java
         int hasilPembagian = mtk.pembagian(21, 2);
-        System.out.println("Pembagian: 21 / 2 = " + hasilPembagian);
+        System.out.println("Pembagian: 21 / 2 = " + hasilPembagian);   // Hasil Output dari ke empat operator diatas 
     }
 }
